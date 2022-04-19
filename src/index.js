660c96228216ef5102fd5c1404af49931605fa97
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-19 11:06:30 -0700

    separate random gif service logic
    Co-authored-by: Meron G. Tekie <mtekiesd210309@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import GifSearch from './js/gif-search';
+import GifSearch from './js/gif-search.js';
+import Gifrandom from './js/random.js';
 
 
 
 $('#search-button').click(function(e) {
   e.preventDefault();
   let keyword = $('#gif-search').val().trim();
+  $('#gif-search').text("");
   let promise = GifSearch.getGif(keyword);
   promise.then(function(response) {
     const body = JSON.parse(response);
@@ -49,31 +51,20 @@ $('#trendy-button').click(function() {
     let src = gif.images.fixed_width.url;
     container += "<img src='" + src + "'>";
   });
-
   $('.trendy-gif-display').html(container);
   }
 });
 
-$('#random-button').click(function() {
-  
-  let request = new XMLHttpRequest();
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=pg`;
-  
-  let response;
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      response = JSON.parse(this.responseText);
-      getElements(response);
-    }
-  };
-
-  request.open("GET", url, true);
-  request.send();
-
-  function getElements(response) {
-    let gifs = response.data.images.fixed_width.url;
+$('#random-button').click(function(e) {
+  e.preventDefault();
+  let promise = Gifrandom.ranGif();
+  promise.then(function(response){
+    const body =JSON.parse(response);
+    let gifs = body.data.images.fixed_width.url;
     let container = "<img src=" + gifs + "></img>";
-  $('.random-gif-display').html(container);
+    $('.random-gif-display').html(container);
+  },function(error){
+    $('.showErrors').text(`There was an error processing your ;request: ${error}`); 
   }
+  );
 });
---
+++ src/js/random.js
@@ -0,0 +1,17 @@
+export default  class Gifrandom {
+  static ranGif() {
+    return new Promise(function (resolve,reject){
+      let request =new XMLHttpRequest();
+      const url =`https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=&rating=pg`;
+      request.onload =function(){
+        if(this.status ===200){
+          resolve(request.response);
+        }else{
+          reject(request.response);
+        }
+      }
+      request.open("GET",url,true);
+      request.send();
+    });
+  }
+}
