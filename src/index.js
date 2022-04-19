b3cff4768d4cb8c817cf0244886697d4da4a730a
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-19 10:15:58 -0700

    separate API sevice logic for searching gifs
    Co-authored-by: Meron G. Tekie <mtekiesd210309@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,57 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import GifSearch from './js/gif-search';
+
+
+
+$('#search-button').click(function(e) {
+  e.preventDefault();
+  let keyword = $('#gif-search').val().trim();
+  let promise = GifSearch.getGif(keyword);
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    let images = body.data;
+    let container = "";
+    images.forEach(function (image) {
+      let src = image.images.fixed_width.url;
+      container += "<img src='" + src + "'>";
+    });
+    $('.search-gif-display').html(container);  
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your ;request: ${error}`);
+    }
+  );
+});
+
+$('#trendy-button').click(function() {
+  
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&rating=pg&lang=en`;
+  
+  let response;
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response) {
+    let gifs = response.data;
+    let container = "";
+    gifs.forEach(function (gif) {
+    let src = gif.images.fixed_width.url;
+    container += "<img src='" + src + "'>";
+  });
+
+  $('.trendy-gif-display').html(container);
+  }
+});
 
 $('#random-button').click(function() {
   
@@ -24,72 +75,5 @@ $('#random-button').click(function() {
     let gifs = response.data.images.fixed_width.url;
     let container = "<img src=" + gifs + "></img>";
   $('.random-gif-display').html(container);
-  }
-});
-
-
-
-
-
-
-
-$('#search-button').click(function(e) {
-  e.preventDefault();
-  let gifSearch = $('#gif-search').val().trim();
-  let request = new XMLHttpRequest();
-
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&limit=5&offset=0&rating=g&lang=en&q=${gifSearch}`;
-
-  let response;
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      response = JSON.parse(this.responseText);
-      getElements(response);
-      console.log(response);
-    }
-  };
-
-  request.open("GET", url, true);
-  request.send();
-
-  function getElements(response) {
-    let images = response.data;
-    let container = "";
-    images.forEach(function (image) {
-    let src = image.images.fixed_width.url;
-    container += "<img src='" + src + "'>";
-  });
-
-  $('.search-gif-display').html(container);
-  }
-});
-
-$('#trendy-button').click(function() {
-  
-  let request = new XMLHttpRequest();
-  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&rating=pg&lang=en`;
-  
-  let response;
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      response = JSON.parse(this.responseText);
-      getElements(response);
-    }
-  };
-
-  request.open("GET", url, true);
-  request.send();
-
-  function getElements(response) {
-    let gifs = response.data;
-    let container = "<ul>";
-    gifs.forEach(function (gif) {
-    let src = gif.images.fixed_width.url;
-    container += "<img src='" + src + "'>";
-  });
-
-  $('.trendy-gif-display').html(container + "</ul>");
   }
 });
---
+++ src/js/gif-search.js
@@ -0,0 +1,17 @@
+export default class GifSearch {
+  static getGif(keyword) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&limit=5&offset=0&rating=g&lang=en&q=${keyword}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
